a0fb1de011bb73affcbe3550f5428c6f28344f65
thefatgother-spec <thefatgother@gmail.com>
2025-08-16 10:23:48 +0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,7 +44,7 @@ const Header: React.FC<HeaderProps> = ({ currentPage, onPageChange }) => {
             {/* Logo */}
             <div className="flex items-center cursor-pointer" onClick={() => onPageChange('home')}>
               <img 
-              src="https://images.pexels.com/photos/3802510/pexels-photo-3802510.jpeg?auto=compress&cs=tinysrgb&w=80&h=80&fit=crop"
+              src="https://hizliresim.com/3va1ex5?auto=compress&cs=tinysrgb&w=80&h=80&fit=crop"
                 />
               <div className="bg-blue-600 text-white p-0 rounded-lg mr-3">
                 <Car className="w-0 h-0" />
