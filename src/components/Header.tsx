eb7be214de21e63ede0da960539764f54ef4e456
thefatgother-spec <thefatgother@gmail.com>
2025-08-16 10:17:18 +0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,7 +46,7 @@ const Header: React.FC<HeaderProps> = ({ currentPage, onPageChange }) => {
               <img 
               src="https://images.pexels.com/photos/3802510/pexels-photo-3802510.jpeg?auto=compress&cs=tinysrgb&w=80&h=80&fit=crop"
                 />
-              
+              <div className="bg-blue-600 text-white p-0 rounded-lg mr-3">
                 <Car className="w-8 h-8" />
               </div>
               <div>
